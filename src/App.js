b7126944a03b3e59f2c9bf1dca27ad2ec5e80a02
David Burt <burt.david.j@gmail.com>
2024-06-06 09:02:52 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GetQuestion from './components/GetQuestion';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <GetQuestion />
+    </div>
+  );
+};
+
+export default App;
